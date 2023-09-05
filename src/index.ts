58ebb270c1ff32a0c9280a5f1181435bad4bbc09
JosephAwuku33 <33jaykay@gmail.com>
2023-09-05 16:59:59 +0000

    setting up the project

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import "dotenv/config";
+import { ApolloServer } from "@apollo/server";
+import { MyContext } from "./types/context.js";
+import { GraphQLError } from "graphql";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { typeDefs } from "./api/graphql/schemas/hostelSchema.js";
+import { resolvers } from "./api/graphql/resolvers/hostelResolver.js";
+import { connectDB } from "./api/config/db.js";
+import userRouter from "./api/routes/userRoutes.js";
+import { getUser } from "./api/util/getUser.js";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const API_PORT = process.env.API_PORT || 4000;
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer<MyContext>({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+connectDB();
+
+const corsOptions = {
+  origin: "*", // Replace with your allowed origin(s)
+  methods: ["GET,HEAD,PUT,PATCH, POST,DELETE"],
+  credentials: true, // Enable cookies and other credentials in CORS requests
+};
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  "/api",
+  cors<cors.CorsRequest>(corsOptions),
+  bodyParser.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => {
+      // Get the user token from the headers.
+      const token = req.headers.authorization?.split(" ")[1] || "";
+
+      // Try to retrieve a user with the token
+      const user = await getUser(token);
+      console.log(user?.first_name);
+
+      // optionally block the user
+      // we could also check user roles/permissions here
+
+      if (!user)
+        // throwing a `GraphQLError` here allows us to specify an HTTP status code,
+        // standard `Error`s will have a 500 status code by default
+        throw new GraphQLError("User is not authenticated", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+            http: { status: 401 },
+          },
+        });
+
+      // Add the user to the context
+      return { user };
+    },
+  })
+);
+
+app.use("/users", userRouter);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: API_PORT }, resolve)
+);
+
+console.log(`🚀 Server ready at http://localhost:4000/api/`);
