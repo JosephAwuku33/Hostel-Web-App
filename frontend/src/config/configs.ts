6ffe2658ad50ec38e8418a10fed2a466264e09a9
JosephAwuku33 <33jaykay@gmail.com>
2023-09-11 16:37:07 +0000

    redux configurations setup

---
+++ frontend/src/config/configs.ts
@@ -0,0 +1,1 @@
+export const URI = 'http://localhost:4000/api';
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './css/index.css';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-// import { Provider } from 'react-redux';
-
-const URI = 'http://localhost:4000/api';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./css/index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { URI } from "./config/configs.ts";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
 const client = new ApolloClient({
   uri: URI,
   cache: new InMemoryCache(),
 });
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-    
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>
+  </React.StrictMode>
+);
