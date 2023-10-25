9bc4fc632fae44e88aee86d74c7026ee3e07eab7
JosephAwuku33 <33jaykay@gmail.com>
2023-10-25 01:49:36 +0000

    removed accidental dist folder

--- dist/api/graphql/resolvers/hostelResolver.js
+++
@@ -1,6 +0,0 @@
-import Rooms from "../../data/models/Rooms.js";
-export const resolvers = {
-    Query: {
-        rooms: async () => await Rooms.find(),
-    }
-};
--- dist/api/util/generateToken.js
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-export const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRATION,
-    });
-};
