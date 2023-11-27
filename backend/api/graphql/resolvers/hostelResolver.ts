61af21d153df86e2416719ae173c554e384f4f7c
JosephAwuku33 <33jaykay@gmail.com>
2023-11-27 01:15:28 +0000

    exception catching

--- backend/api/graphql/resolvers/hostelResolver.ts
+++ backend/api/graphql/resolvers/hostelResolver.ts
@@ -2,6 +2,16 @@ import Rooms from "../../data/models/Rooms.js";
 
 export const resolvers = {
   Query: {
-    rooms: async () => await Rooms.find() ,
-  }
+    rooms: async () => {
+      try {
+        const rooms = await Rooms.find();
+        return rooms;
+      } catch (err) {
+        throw new Error("Error fetching rooms from database");
+      }
+    }
+  },
+
+  
+  
 };
