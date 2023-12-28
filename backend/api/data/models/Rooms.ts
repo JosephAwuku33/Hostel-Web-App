c4fbf240d90c490149439b676057986559d1c91d
JosephAwuku33 <33jaykay@gmail.com>
2023-12-28 13:32:38 +0000

    room validations

--- backend/api/data/models/Rooms.ts
+++ backend/api/data/models/Rooms.ts
@@ -4,31 +4,37 @@ import mongoose from "mongoose";
 const roomSchema = new mongoose.Schema({
     number: {
         type: Number,
+        required: true,
         default: null
     },
 
     status: {
         type: String, 
+        required: true,
         default: null
     },
 
     type: { 
         type: String,
+        required: true,
         default: null,
     },
 
     occupants: { 
         type: Number,
+        required: true,
         default: null, 
     },
 
     price: {
         type: Number,
+        required: true,
         default: null,
     },
 
     gender_type: {
         type: String,
+        required: true,
         default: null,
     }
 });
