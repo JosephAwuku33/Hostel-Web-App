09f6cf6995c0de70dcdb3b0b7c122ea433b282e8
JosephAwuku33 <33jaykay@gmail.com>
2023-12-28 13:22:03 +0000

    adding a bit of validations

--- backend/api/data/models/User.ts
+++ backend/api/data/models/User.ts
@@ -4,21 +4,25 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     first_name: {
         type: String,
+        required: true,
         default: null
     },
 
     last_name: {
         type: String, 
+        required: true,
         default: null
     },
 
     email: { 
         type: String,
+        required: true,
         unique: true
     },
 
     password: { 
-        type: String 
+        type: String, 
+        required: true
     },
 });
 
